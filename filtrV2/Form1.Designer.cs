4b24ba2c9926d5d67199cb431a8cee4aca57f3a2
381706-2BandenkovD <daniilbandenkov@gmail.com>
2019-02-09 00:18:06 +0300

    omae

---
+++ filtrV2/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace filtrV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.создатьToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.открытьToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.сохранитьToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.справкаToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.pointsFiltersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matrixFiltersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negativeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smurfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.posterizationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackwhiteOnlyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackwhiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.brightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sharpnesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.originalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(561, 267);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripButton,
+            this.открытьToolStripButton,
+            this.сохранитьToolStripButton,
+            this.toolStripButton1,
+            this.справкаToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(573, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // создатьToolStripButton
+            // 
+            this.создатьToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.создатьToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("создатьToolStripButton.Image")));
+            this.создатьToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.создатьToolStripButton.Name = "создатьToolStripButton";
+            this.создатьToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.создатьToolStripButton.Text = "&Создать";
+            // 
+            // открытьToolStripButton
+            // 
+            this.открытьToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.открытьToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("открытьToolStripButton.Image")));
+            this.открытьToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.открытьToolStripButton.Name = "открытьToolStripButton";
+            this.открытьToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.открытьToolStripButton.Text = "&Открыть";
+            this.открытьToolStripButton.Click += new System.EventHandler(this.открытьToolStripButton_Click);
+            // 
+            // сохранитьToolStripButton
+            // 
+            this.сохранитьToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.сохранитьToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("сохранитьToolStripButton.Image")));
+            this.сохранитьToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.сохранитьToolStripButton.Name = "сохранитьToolStripButton";
+            this.сохранитьToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.сохранитьToolStripButton.Text = "&Сохранить";
+            this.сохранитьToolStripButton.Click += new System.EventHandler(this.сохранитьToolStripButton_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pointsFiltersToolStripMenuItem,
+            this.matrixFiltersToolStripMenuItem,
+            this.originalToolStripMenuItem});
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(32, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // справкаToolStripButton
+            // 
+            this.справкаToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.справкаToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("справкаToolStripButton.Image")));
+            this.справкаToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.справкаToolStripButton.Name = "справкаToolStripButton";
+            this.справкаToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.справкаToolStripButton.Text = "Спр&авка";
+            // 
+            // pointsFiltersToolStripMenuItem
+            // 
+            this.pointsFiltersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.negativeToolStripMenuItem,
+            this.smurfToolStripMenuItem,
+            this.posterizationToolStripMenuItem,
+            this.blackwhiteOnlyToolStripMenuItem,
+            this.blackwhiteToolStripMenuItem,
+            this.brightToolStripMenuItem});
+            this.pointsFiltersToolStripMenuItem.Name = "pointsFiltersToolStripMenuItem";
+            this.pointsFiltersToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pointsFiltersToolStripMenuItem.Text = "point filters";
+            // 
+            // matrixFiltersToolStripMenuItem
+            // 
+            this.matrixFiltersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blurToolStripMenuItem,
+            this.sharpnesToolStripMenuItem});
+            this.matrixFiltersToolStripMenuItem.Name = "matrixFiltersToolStripMenuItem";
+            this.matrixFiltersToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.matrixFiltersToolStripMenuItem.Text = "matrix filters";
+            // 
+            // negativeToolStripMenuItem
+            // 
+            this.negativeToolStripMenuItem.Name = "negativeToolStripMenuItem";
+            this.negativeToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.negativeToolStripMenuItem.Text = "negative";
+            this.negativeToolStripMenuItem.Click += new System.EventHandler(this.negativeToolStripMenuItem_Click);
+            // 
+            // smurfToolStripMenuItem
+            // 
+            this.smurfToolStripMenuItem.Name = "smurfToolStripMenuItem";
+            this.smurfToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.smurfToolStripMenuItem.Text = "smurf";
+            this.smurfToolStripMenuItem.Click += new System.EventHandler(this.smurfToolStripMenuItem_Click);
+            // 
+            // posterizationToolStripMenuItem
+            // 
+            this.posterizationToolStripMenuItem.Name = "posterizationToolStripMenuItem";
+            this.posterizationToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.posterizationToolStripMenuItem.Text = "posterization";
+            this.posterizationToolStripMenuItem.Click += new System.EventHandler(this.posterizationToolStripMenuItem_Click);
+            // 
+            // blackwhiteOnlyToolStripMenuItem
+            // 
+            this.blackwhiteOnlyToolStripMenuItem.Name = "blackwhiteOnlyToolStripMenuItem";
+            this.blackwhiteOnlyToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.blackwhiteOnlyToolStripMenuItem.Text = "black-white only";
+            this.blackwhiteOnlyToolStripMenuItem.Click += new System.EventHandler(this.blackwhiteOnlyToolStripMenuItem_Click);
+            // 
+            // blackwhiteToolStripMenuItem
+            // 
+            this.blackwhiteToolStripMenuItem.Name = "blackwhiteToolStripMenuItem";
+            this.blackwhiteToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.blackwhiteToolStripMenuItem.Text = "black-white";
+            this.blackwhiteToolStripMenuItem.Click += new System.EventHandler(this.blackwhiteToolStripMenuItem_Click);
+            // 
+            // brightToolStripMenuItem
+            // 
+            this.brightToolStripMenuItem.Name = "brightToolStripMenuItem";
+            this.brightToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.brightToolStripMenuItem.Text = "bright";
+            this.brightToolStripMenuItem.Click += new System.EventHandler(this.brightToolStripMenuItem_Click);
+            // 
+            // blurToolStripMenuItem
+            // 
+            this.blurToolStripMenuItem.Name = "blurToolStripMenuItem";
+            this.blurToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.blurToolStripMenuItem.Text = "blur";
+            this.blurToolStripMenuItem.Click += new System.EventHandler(this.blurToolStripMenuItem_Click);
+            // 
+            // sharpnesToolStripMenuItem
+            // 
+            this.sharpnesToolStripMenuItem.Name = "sharpnesToolStripMenuItem";
+            this.sharpnesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sharpnesToolStripMenuItem.Text = "sharpness";
+            this.sharpnesToolStripMenuItem.Click += new System.EventHandler(this.sharpnesToolStripMenuItem_Click);
+            // 
+            // originalToolStripMenuItem
+            // 
+            this.originalToolStripMenuItem.Name = "originalToolStripMenuItem";
+            this.originalToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.originalToolStripMenuItem.Text = "original";
+            this.originalToolStripMenuItem.Click += new System.EventHandler(this.originalToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 307);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton создатьToolStripButton;
+        private System.Windows.Forms.ToolStripButton открытьToolStripButton;
+        private System.Windows.Forms.ToolStripButton сохранитьToolStripButton;
+        private System.Windows.Forms.ToolStripButton справкаToolStripButton;
+        private System.Windows.Forms.ToolStripSplitButton toolStripButton1;
+        private System.Windows.Forms.ToolStripMenuItem pointsFiltersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem negativeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smurfToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matrixFiltersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem posterizationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackwhiteOnlyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackwhiteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem brightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blurToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sharpnesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem originalToolStripMenuItem;
+    }
+}
+
---
+++ filtrV2/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filtrV2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap MyImg;
+        Bitmap OrImg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpg; *.bmp | All Files (*.*) | *.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                MyImg = new Bitmap(dialog.FileName);
+                OrImg = new Bitmap(dialog.FileName);
+                pictureBox1.Image = MyImg;
+                pictureBox1.Refresh();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void сохранитьToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void negativeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    Color ReversedColor = MyImg.GetPixel(i, j);
+                    ReversedColor = Color.FromArgb(255 - ReversedColor.R, 255 - ReversedColor.G, 255 - ReversedColor.B);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        private void originalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyImg = OrImg;
+            pictureBox1.Image = MyImg;
+            pictureBox1.Refresh();
+        }
+
+        private void smurfToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    Color ReversedColor = MyImg.GetPixel(i, j);
+                    ReversedColor = Color.FromArgb(ReversedColor.B, ReversedColor.G, ReversedColor.R);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        private void posterizationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    Color ReversedColor = MyImg.GetPixel(i, j);
+                    const int val = 6;
+                    ReversedColor = Color.FromArgb(((ReversedColor.R >> val) << val) + 32, ((ReversedColor.G >> val) << val) + 32, ((ReversedColor.B >> val) << val) + 32);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        private void blackwhiteOnlyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    Color ReversedColor = MyImg.GetPixel(i, j);
+                    if (ReversedColor.B + ReversedColor.G + ReversedColor.R > 325)
+                        ReversedColor = Color.FromArgb(255, 255, 255);
+                    else
+                        ReversedColor = Color.FromArgb(0, 0, 0);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        private void blackwhiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    Color ReversedColor = MyImg.GetPixel(i, j);
+                    int col = (ReversedColor.B + ReversedColor.G + ReversedColor.R) / 3;
+                    ReversedColor = Color.FromArgb(col, col, col);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        private void brightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyImg = OrImg;
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    Color ReversedColor = MyImg.GetPixel(i, j);
+                    int colR = ReversedColor.R * 2;
+                    if (colR > 255)
+                        colR = 255;
+                    int colB = ReversedColor.B * 2;
+                    if (colB > 255)
+                        colB = 255;
+                    int colG = ReversedColor.G * 2;
+                    if (colG > 255)
+                        colG = 255;
+                    ReversedColor = Color.FromArgb(colR, colG, colB);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        private void blurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    BlurFilter MyBlur = new BlurFilter();
+                    Color ReversedColor = MyBlur.CalculateNewClolor(MyImg, i, j);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        private void sharpnesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MyImg.Width; i++)
+                for (int j = 0; j < MyImg.Height; j++)
+                {
+                    SharpnessFilter MySharp = new SharpnessFilter();
+                    Color ReversedColor = MySharp.CalculateNewClolor(MyImg, i, j);
+                    MyImg.SetPixel(i, j, ReversedColor);
+                }
+            pictureBox1.Refresh();
+        }
+
+        class MatrixFilter
+        {
+            protected float[,] kernel = null;
+            public Color CalculateNewClolor(Bitmap img, int x, int y)
+            {
+                int Radx = kernel.GetLength(0) / 2;
+                int Rady = kernel.GetLength(1) / 2;
+                float ResR = 0;
+                float ResG = 0;
+                float ResB = 0;
+                for (int i = -Rady; i <= Rady; i++)
+                    for (int j = -Radx; j <= Radx; j++)
+                    {
+                        int X = Math.Min(x + j, img.Width - 1);
+                        X = Math.Max(X, 0);
+                        int Y = Math.Min(y + i, img.Height - 1);
+                        Y = Math.Max(Y, 0);
+                        Color NeighborColor = img.GetPixel(X, Y);
+                        ResR += NeighborColor.R * kernel[j + Radx, i + Rady];
+                        ResG += NeighborColor.G * kernel[j + Radx, i + Rady];
+                        ResB += NeighborColor.B * kernel[j + Radx, i + Rady];
+                    }
+                ResR = Math.Min(ResR, 255);
+                ResR = Math.Max(ResR, 0);
+                ResG = Math.Min(ResG, 255);
+                ResG = Math.Max(ResG, 0);
+                ResB = Math.Min(ResB, 255);
+                ResB = Math.Max(ResB, 0);
+                return Color.FromArgb((int)ResR, (int)ResG, (int)ResB);
+            }
+        }
+
+        class BlurFilter : MatrixFilter
+        {
+            public BlurFilter()
+            {
+                int sizeX = 3;
+                int sizeY = 3;
+                kernel = new float[sizeX, sizeY];
+                for (int i = 0; i < sizeX; i++)
+                    for (int j = 0; j < sizeY; j++)
+                        kernel[i, j] = 1.0f / (float)(sizeX * sizeY);
+            }
+        }
+
+        class SharpnessFilter : MatrixFilter
+        {
+            public SharpnessFilter()
+            {
+                int sizeX = 3;
+                int sizeY = 3;
+                kernel = new float[sizeX, sizeY];
+                for (int i = 0; i < sizeX; i++)
+                    for (int j = 0; j < sizeY; j++)
+                        kernel[i, j] = -1.0f / 2.0f;
+                kernel[1, 1] = 1.0f / 2.0f * sizeX * sizeY + 1.0f / 2.0f;
+            }
+        }
+    }
+}
